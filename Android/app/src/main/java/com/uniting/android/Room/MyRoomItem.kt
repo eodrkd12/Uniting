34cee4f587d4b83617a1fd4ba372d7180cde7e2b
eodrkd12 <37127443+eodrkd12@users.noreply.github.com>
2020-08-24 17:56:15 +0900

    ㅁㄴㅇㄹ

---
+++ Android/app/src/main/java/com/uniting/android/Room/MyRoomItem.kt
@@ -0,0 +1,7 @@
+package com.uniting.android.Room
+
+import com.uniting.android.DB.Entity.Room
+
+data class MyRoomItem (
+    var room : Room
+)
---
+++ Android/app/src/main/java/com/uniting/android/Room/RoomListActivity.kt
@@ -0,0 +1,12 @@
+package com.uniting.android.Room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uniting.android.R
+
+class RoomListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room_list)
+    }
+}
