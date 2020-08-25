bfd5a6cfd89e42ebfea51cb3cf469281d0a8c2c2
eodrkd12 <37127443+eodrkd12@users.noreply.github.com>
2020-08-25 16:04:01 +0900

    dfs

--- Android/app/src/main/java/com/uniting/android/Class/PSDialog.kt
+++ Android/app/src/main/java/com/uniting/android/Class/PSDialog.kt
@@ -1,6 +1,54 @@
 package com.uniting.android.Class
 
 import android.app.Activity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.uniting.android.R
+import com.uniting.android.Room.MakeRoomActivity
 
 class PSDialog(activity: Activity) {
+
+    class BottomSheetDialog() : BottomSheetDialogFragment() {
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            super.onCreateView(inflater, container, savedInstanceState)
+            return inflater.inflate(R.layout.dialog_category, container, false)
+        }
+
+        override fun onActivityCreated(savedInstanceState: Bundle?) {
+            super.onActivityCreated(savedInstanceState)
+            view?.findViewById<TextView>(R.id.text_hobby)?.setOnClickListener {
+                MakeRoomActivity.category="취미"
+                dismiss()
+            }
+
+            view?.findViewById<TextView>(R.id.text_study)?.setOnClickListener {
+                MakeRoomActivity.category="스터디"
+                dismiss()
+            }
+
+            view?.findViewById<TextView>(R.id.text_recruit)?.setOnClickListener {
+                MakeRoomActivity.category="취/창업"
+                dismiss()
+            }
+
+            view?.findViewById<TextView>(R.id.text_counsel)?.setOnClickListener {
+                MakeRoomActivity.category="고민상담"
+                dismiss()
+            }
+
+            view?.findViewById<TextView>(R.id.text_talk)?.setOnClickListener {
+                MakeRoomActivity.category="아무말"
+                dismiss()
+            }
+        }
+    }
 }
