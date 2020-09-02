a2ad64dee25944ba6d152fea91a52aba9ff49007
sorrylim <52535550+sorrylim@users.noreply.github.com>
2020-09-02 16:31:27 +0900

    2020.09.02
    16:31

---
+++ Android/app/src/main/java/com/uniting/android/Login/Signup1Activity.kt
@@ -0,0 +1,12 @@
+package com.uniting.android.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uniting.android.R
+
+class Signup1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup1)
+    }
+}
